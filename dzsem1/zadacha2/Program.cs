3d56a13941214513b7c89907828958c43e9a063a
Maria <kalelm@mail.ru>
2022-09-02 23:40:10 +0300

    add dzsem1

---
+++ dzsem1/zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+ Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b) Console.WriteLine("Max = "+a);
+            if (a < b) Console.WriteLine("Max = " + b);
+            if (a == b) Console.WriteLine("Нет максимального, числа равны");
---
+++ dzsem1/zadacha4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+  Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            int c = Convert.ToInt32(Console.ReadLine());
+ 
+            int max=a;
+            if ((a == b) && (a == c))
+            {
+                Console.WriteLine("Нет максимального, числа равны");
+            }
+            else
+            {
+                if (a > b) max = a;
+                else max = b;
+                if (c > max) Console.WriteLine("Max = " + c);
+                else Console.WriteLine("Max = " + max);
+            }
+ 
